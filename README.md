17c7392d3a843fcc01855b8cd6fdbc4ab8facda7
ariedro <ariedro@gmail.com>
2022-02-23 18:29:48 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# SMTP Faker
+
+Service for faking a SMTP server for capturing emails with file attachment and redirecting them to a telegram bot 
+
+## Install
+
+```
+$ npm install
+```
+
+## Config
+
+Edit the `config.json` file to the corresponding values
+
+## Run
+
+```
+$ npm run start
+```
---
+++ faker.js
@@ -0,0 +1,52 @@
+const net = require("net");
+const simpleParser = require("mailparser").simpleParser;
+const TelegramBot = require("node-telegram-bot-api");
+const config = require("./config.json");
+
+const bot = new TelegramBot(config.tg.token);
+
+let handlingData = false;
+let fileBuffer = "";
+
+function handleData(data, socket) {
+  const sData = data.toString();
+  const request = sData.slice(0, 4);
+  switch (request) {
+    case "EHLO":
+    case "MAIL":
+    case "RCPT":
+      socket.write("250 OK\n");
+      break;
+    case "DATA":
+      socket.write("354 Start mail input\n");
+      handlingData = true;
+      break;
+    case "QUIT":
+      socket.write("221 Bye\n");
+      break;
+  }
+  if (handlingData) {
+    fileBuffer += sData;
+    if (sData.match("\r\n.\r\n")) {
+      socket.write("250 OK\n");
+      handleFile(fileBuffer);
+      fileBuffer = "";
+      handlingData = false;
+    }
+  }
+}
+
+function handleFile(file) {
+  simpleParser(file, {}, (_err, parsed) =>
+    parsed.attachments.forEach(({ content }) =>
+      bot.sendPhoto(config.tg.chatId, content)
+    )
+  );
+}
+
+net
+  .createServer(function (socket) {
+    socket.write("220 Email 100% real no fake\n");
+    socket.on("data", (data) => handleData(data, socket));
+  })
+  .listen(config.server.port, config.server.ip);
